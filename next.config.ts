ddfc3fa5e1f47b8fd7e8a0487148a28c023aeac1
Joe Conlin <joeconlin@joes-air-m4.lan>
2026-01-17 15:37:20 +0800

    Fix Next.js serverActions config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  serverActions: {
-    bodySizeLimit: "2mb",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
   },
 };
 
